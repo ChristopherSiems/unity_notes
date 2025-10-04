d7934b8140d497f7e1af7fd74e0c17e532482c0a
n1b11 <nburd@clarku.edu>
2025-10-04 02:31:18 -0400

    Add aggregation file

---
+++ client-backend/main-server/aggregation/aggregation.py
@@ -0,0 +1,25 @@
+class AggregationAgent: 
+
+    def __init__(self): 
+        pass 
+
+    def generate_aggregate(self, notes: list[str]) -> str: 
+
+        #cluster 
+
+        #summarize each
+
+        #summarize all 
+        pass 
+    
+    
+    def summarize(self, notes: list[str], max_size:int, output_size:int) -> str: 
+        #summarizes collection of notes
+
+        #if to many notes: summary = aggregate(Recurse multiple groups) 
+
+        pass
+    
+    def aggregate(self, summaries: list[str]) -> str: 
+        #takes a group of summaries and combines them
+        pass 
